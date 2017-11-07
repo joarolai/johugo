baaf328080171076bc3eb2fb4d095d2e8cd9e57e
Joar Rusten <joarolai@gmail.com>
2017-11-07 12:02:38 +0100

    Cleanup tabs to space

--- static/js/custom.js
+++ static/js/custom.js
@@ -15,12 +15,12 @@ $('.navbar-collapse ul li a').click(function() {
 });
 
 $('div.modal').on('show.bs.modal', function() {
-	var modal = this;
-	var hash = modal.id;
-	window.location.hash = hash;
-	window.onhashchange = function() {
-		if (!location.hash){
-			$(modal).modal('hide');
-		}
-	}
+  var modal = this;
+  var hash = modal.id;
+  window.location.hash = hash;
+  window.onhashchange = function() {
+    if (!location.hash){
+      $(modal).modal('hide');
+    }
+  }
 });
